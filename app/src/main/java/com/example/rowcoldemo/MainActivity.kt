a42854e5b98e7fb932b2ce148207bf42ab933fec
ichiil <bloodgamer776@gmail.com>
2025-11-17 05:50:12 +0700

    Восьмой дизайн

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -49,9 +49,9 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(horizontalArrangement = Arrangement.End,
-        modifier = modifier.size(width = 400.dp, height = 200.dp)) {
-        TextCell("1")
+    Column(verticalArrangement = Arrangement.Bottom,
+        modifier = modifier.height(400.dp)) {
+    TextCell("1")
         TextCell("2")
         TextCell("3")
     }
